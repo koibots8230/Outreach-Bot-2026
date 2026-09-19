cd06504d19f467196e5b9eb8d67e4f2334f2aaec
Sebastian <sebastianmseguin@gmail.com>
2026-09-18 20:19:02 -0400

    Fixed coding convention issues, re-applied spotless and rebuilt.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.*;
-import frc.robot.commands.intakeCommands;
+import frc.robot.commands.IntakeCommands;
 import frc.robot.subsystems.*;
 
 @Logged
@@ -32,7 +32,7 @@ public class RobotContainer {
 
   private void configureBindings() {
     Trigger intakeButton = new Trigger(() -> controller.getLeftTriggerAxis() > 0.15);
-    intakeButton.onTrue(intakeCommands.startIntake(spindexer));
+    intakeButton.onTrue(IntakeCommands.startIntake(spindexer));
     intakeButton.onFalse(spindexer.setVelocityCommand(RPM.of(0)));
 
     Trigger intakeReverse = new Trigger(() -> controller.getRightTriggerAxis() > 0.15);
--- src/main/java/frc/robot/commands/intakeCommands.java
+++ src/main/java/frc/robot/commands/intakeCommands.java
@@ -1,14 +1,11 @@
 package frc.robot.commands;
 
-import static edu.wpi.first.units.Units.RPM;
-
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Constants.SpindexerConstants;
 import frc.robot.subsystems.Spindexer;
 
-public class intakeCommands {
-    public static Command startIntake(Spindexer spindexer) {
-        return spindexer.setVelocityCommand(SpindexerConstants.INTAKE_VELOCITY);
-    }
+public class IntakeCommands {
+  public static Command startIntake(Spindexer spindexer) {
+    return spindexer.setVelocityCommand(SpindexerConstants.INTAKE_VELOCITY);
+  }
 }
